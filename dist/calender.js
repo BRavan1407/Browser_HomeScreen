21e25e16c6e6111bb4ee7560284e2d7e15db4104
BRavan1407 <upadhyaypruthviraj131@gmail.com>
2024-06-19 18:46:39 +0530

    19-06-2024

--- dist/calender.js
+++ dist/calender.js
@@ -1,19 +1,63 @@
 import dateZone from "./clock.js";
 // console.log(allEvents);
 const currentDate = new Date();
-console.log(`${currentDate.getDate()}-${currentDate.getMonth().toString().padStart(2, '0')}-${currentDate.getFullYear()}`);
+
+// localStorage.clear();
 var allEvents = [];
+allEvents = JSON.parse(localStorage.getItem('allEvents') || "[]");
+
+
 
 // currentDate.setMonth(1);
 // currentDate.setFullYear(2012)
 
 var uniqueId;
 
+// Generating unique ID
 function createID (){
     uniqueId = new Date().getTime();
     return uniqueId;
 }
 
+// Saving to local Storage
+function saveEvents(){
+    // try{
+    //     localStorage.setItem('allEvents', JSON.stringify(allEvents));
+    //     console.log('saved to local storage');
+    // }
+    // catch (err){
+    //     console.log('Error while saving to local storage...',err);
+    // }
+
+    try{
+        //let existingEvents =JSON.parse(localStorage.getItem('allEvents') || "[]") ;
+
+        //existingEvents.forEach(newEvent => allEvents.push(newEvent));
+
+        localStorage.setItem('allEvents', JSON.stringify(allEvents));
+    }
+    catch(err){
+        console.log('Error while saving', err);
+    }
+}
+
+// Retrive Task
+function retriveEvents(){
+    let eventTasks = localStorage.getItem("allEvents");
+
+    return eventTasks ? JSON.parse(eventTasks) : [];
+}
+console.log(retriveEvents());
+
+// Delete Events
+window.deleteEvent = (clickedDate) =>{ 
+    allEvents.forEach(some => {
+        console.log(some.id);
+    })
+    
+}
+
+// Rendering Calendar
 window.renderCalendar = () => {
     const daysInmonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
     // console.log(daysInmonth);
@@ -52,31 +96,30 @@ window.renderCalendar = () => {
     // Show Calendar
     let span = '';
 
-    // Set first day of month
-
+    // Prev Month Disabled Dates
     for (let i = days; i > 0; i--) {
         span += `<span class="date text-center text-2xl text-gray-500">${(firstDay - i) + 1}</span>`
     }
 
+    // Running month Dates
     for (let i = 1; i <= daysInmonth; i++) {
-        // let selectedDate = `${currentDate.getFullYear()}-${(currentDate.getMonth()+1)}-${currentDate.getDate()}`;
+    let selectedDate = `${currentDate.getFullYear()}-${(currentDate.getMonth()+1).toString().padStart(2, '0')}-${i.toString().padStart(2, '0')}`;
+        // let selectedDate = `hello`;
         if (i == today.getDate() && currentDate.getMonth() == today.getMonth() && currentDate.getFullYear() == today.getFullYear()) {
-            span += `<span class="date text-center text-2xl text-white today cursor-pointer" onclick = 'openEvent(${currentDate.getDate()}-${currentDate.getMonth().toString().padStart(2, '0')}-${currentDate.getFullYear()})'>${i}</span>`;
+            span += `<span class="date text-center text-2xl text-white today cursor-pointer" onclick = 'openEvent("${selectedDate}")'>${i}</span>`;
         }
         else {
-            span += `<span class="date text-center text-2xl text-white cursor-pointer" onclick = 'openEvent(${currentDate.getDate()}-${currentDate.getMonth().toString().padStart(2, '0')}-${currentDate.getFullYear()})'>${i}</span>`;
+            span += `<span class="date text-center text-2xl text-white cursor-pointer" onclick = 'openEvent("${selectedDate}")'>${i}</span>`;
         }
-    // console.log(selectedDate);
-
     }
 
+    // Next Month Disabled dates
     for (let i = 1; i <= (6 - lastDay); i++) {
         span += `<span class="date text-center text-2xl text-gray-500">${i}</span>`
     }
 
+    // Display calender
     document.getElementById('calendarDates').innerHTML = span;
-
-
 }
 
 // Add Buttons of Month changer
@@ -90,14 +133,16 @@ window.changeMonth = (btn) => {
     renderCalendar();
 }
 
-window.openEvent = (date) => {
-    console.log(date)
+// Open Event Planner
+window.openEvent = (clickedDate) => {
+    // console.log('Clicked Date is :',clickedDate);
     document.querySelector('.event_planner').classList.remove('hidden');
-    // if(selectedDate){
-    //     document.getElementById('event_date').value = selectedDate;
-    // }
+    if(clickedDate){
+        document.getElementById('event_date').value = clickedDate;
+    }
 }
 
+// For Closing eventPlanner Popup
 window.closeEvent = () => {
     document.querySelector('.event_planner').classList.add('hidden');
 }
@@ -108,6 +153,7 @@ document.addEventListener('keydown', function(event){
     }
 })
 
+// Adding Event
 window.addEvent = () => {
     const dt = new dateZone();
 
@@ -116,41 +162,13 @@ window.addEvent = () => {
     let eventTime = document.getElementById('event_time').value;
     let id = createID();
 
-    // allEvents.push({id : id, eventContent : eventContent, eventDate : eventDate, eventTime : eventTime});
+    allEvents.push({id : id, eventContent : eventContent, eventDate : eventDate, eventTime : eventTime});
 
-    // console.log(typeof eventDate);
     console.log(allEvents);
-    try{
-        localStorage.setItem('allEvents', JSON.stringify(allEvents));
-        console.log('saved to local storage');
-    }
-    catch (err){
-        console.log('Error while saving to local storage...',err);
-    }
-    // saveEvents();
+    
+    saveEvents();
     closeEvent();
     
-    allEvents.push({id : id, eventContent : eventContent, eventDate : eventDate, eventTime : eventTime});
-    
     document.getElementById('event').value = '';
-    // document.getElementById('event_date').value = '';
     document.getElementById('event_time').value = '';
 }
-
-// function getToday(){
-//     console.log('');
-// }
-
-function retriveEvents(){
-    let eventTasks = localStorage.getItem("allEvents");
-
-    return JSON.parse(eventTasks);
-}
-
-console.log(retriveEvents());
-console.log(localStorage);
-// localStorage.clear();
-// function deleteEvent(){
-//     console.log(`Deleting task of ${id}`);
-// }
-// console.log(deleteEvent());
